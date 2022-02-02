91b8de5601c22a407bffba4c392fb36d4793ce4c
axel <axelbruget@gmail.com>
2022-02-02 21:05:49 +0100

    Added xDebug config + login configuration, it's been a long time guys since last time. I am back at work now and the project is going to be huge in the next week. STAY TUNED BOYS

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="")
+     * @Route("/", name="app_homepage")
      */
     public function index()
     {
